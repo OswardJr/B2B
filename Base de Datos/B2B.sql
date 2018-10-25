0039ad923e090e1d86e30ce661ff41c3af04b487
Osward José <ojpr15@gmail.com>
2018-10-25 17:20:24 -0430

    'bd-actual'

--- Base de Datos/B2B.sql
+++ Base de Datos/B2B.sql
@@ -11,7 +11,45 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+
+-- Volcando estructura de base de datos para b2b
+CREATE DATABASE IF NOT EXISTS `b2b` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `b2b`;
+
+-- Volcando estructura para tabla b2b.referido
+CREATE TABLE IF NOT EXISTS `referido` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(50) DEFAULT NULL,
+  `Apellido` varchar(50) DEFAULT NULL,
+  `DNI` int(11) NOT NULL,
+  `Fecha_nacimiento` date DEFAULT NULL,
+  `Edad` int(11) DEFAULT NULL,
+  `sexo` varchar(50) DEFAULT NULL,
+  `Estado_civil` varchar(50) DEFAULT NULL,
+  `Formacion` varchar(50) DEFAULT NULL,
+  `Direccion` varchar(50) DEFAULT NULL,
+  `Distrito` varchar(50) DEFAULT NULL,
+  `Provincia` varchar(50) DEFAULT NULL,
+  `Region` varchar(50) DEFAULT NULL,
+  `Telefono` int(11) DEFAULT NULL,
+  `Tel-fijo` int(11) DEFAULT NULL,
+  `Correo` varchar(50) DEFAULT NULL,
+  `Codigo` int(11) DEFAULT NULL,
+  `URL` varchar(50) DEFAULT NULL,
+  `Nivel_usuario` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- La exportación de datos fue deseleccionada.
+-- Volcando estructura para tabla b2b.usuarios
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_usuario` varchar(50) NOT NULL,
+  `clave` varchar(50) NOT NULL,
+  `tipo_user` enum('0','1') DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- La exportación de datos fue deseleccionada.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
